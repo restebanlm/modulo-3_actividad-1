4839e4cc4962e259dc7f6d1571e0e5c97c8b5da6
Esteban <49765124+restebanlm@users.noreply.github.com>
2021-05-31 22:17:32 -0300

    Actualizo 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-function App() {
-  return (
-    <>
-    </>
-  );
-}
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { NavBar } from "./views/NavBar";
+import { JobsView } from "./views/JobsView";
+import { CountriesView } from "./views/CountriesView";
+import { CitiesView } from "./views/CitiesView";
+import { CompaniesView } from "./views/CompaniesView";
+import { NotFoundView } from "./views/NotFoundView";
+
+const App = () => (
+  <div className="App">
+    <NavBar />
+    <Switch>
+      <Route path="/" exact component={JobsView} />
+      <Route path="/countries" exact component={CountriesView} />
+      <Route path="/cities" exact component={CitiesView} />
+      <Route path="/companies" exact component={CompaniesView} />
+      <Route component={NotFoundView} />
+    </Switch>
+  </div>
+);
 
 export default App;
